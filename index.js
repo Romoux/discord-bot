48d666c5dd72b1426eca36e1b37145529bb43863
Romoux <87906509+Romoux@users.noreply.github.com>
2021-08-06 00:47:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const prefix = data.prefix
 //Token\\
 Bot.login(process.env.TOKEN);
 
+//Port\\
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
 //Connection au bot + status\\
 Bot.on('ready', () => {
